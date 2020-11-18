e2d127ad23939fab5dc04793146cc59a2a543fdb
Erik Portillo <erikportillo1@gmail.com>
2020-11-18 13:18:12 -0500

    Created a variable called pokemonList
    I created a new variable and added data into the array

---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+let pokemonList = [{
+    name: 'Bulbasaur',
+    height: '0.7',
+    type: ['grass', 'poison'],
+  },
+  {
+    name: 'Ivysaur',
+    height: '1.0',
+    type: ['grass', 'poison'],
+  },
+  {
+    name: 'Venusaur',
+    height: '2.0',
+    type: ['grass', 'poison'],
+  },
+  {
+    name: 'Charmander',
+    height: '0.6',
+    type: ['fire'],
+  },
+  {
+    name: 'Charmeleon',
+    height: '1.1',
+    type: ['fire'],
+  },
+  {
+    name: 'Charizard',
+    height: '1.7',
+    type: ['fire'],
+  },
+  {
+    name: 'Squirtle',
+    height: '0.5',
+    type: ['water'],
+  },
+  {
+    name: 'Wartortle',
+    height: '1.0',
+    type: ['water'],
+  },
+  {
+    name: 'Blastoise',
+    height: '1.6',
+    type: ['water'],
+  },
+]
