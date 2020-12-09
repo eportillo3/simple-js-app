462231d7199a5752ccf1b850b2345f8aab8bcf6d
Erik Portillo <erikportillo1@gmail.com>
2020-12-09 12:15:12 -0500

    Updated scripts.js
    I removed the static list and used an API to retreive the pokemon list of 150 pokemons

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,6 @@
-let pokemonRepository = (function () {
-  let pokemonList = [{
-    name: 'Bulbasaur',
-    height: '0.7',
-    type: ['grass', 'poison'],
-  },
-  {
-    name: 'Ivysaur',
-    height: '1.0',
-    type: ['grass', 'poison'],
-  },
-  {
-    name: 'Venusaur',
-    height: '2.0',
-    type: ['grass', 'poison'],
-  },
-  {
-    name: 'Charmander',
-    height: '0.6',
-    type: ['fire'],
-  },
-  {
-    name: 'Charmeleon',
-    height: '1.1',
-    type: ['fire'],
-  },
-  {
-    name: 'Charizard',
-    height: '1.7',
-    type: ['fire'],
-  },
-  {
-    name: 'Squirtle',
-    height: '0.5',
-    type: ['water'],
-  },
-  {
-    name: 'Wartortle',
-    height: '1.0',
-    type: ['water'],
-  },
-  {
-    name: 'Blastoise',
-    height: '1.6',
-    type: ['water'],
-  }
-];
+let pokemonRepository = (function() {
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
   function add(pokemon) {
     pokemonList.push(pokemon);
@@ -54,7 +10,9 @@ let pokemonRepository = (function () {
     return pokemonList;
   }
 
-  function addListItem(pokemon){
+
+
+  function addListItem(pokemon) {
     let pokemonList = document.querySelector('.pokemon-list');
     let listItem = document.createElement('li');
     let button = document.createElement('button');
@@ -62,39 +20,66 @@ let pokemonRepository = (function () {
     button.classList.add('button-class');
     listItem.appendChild(button);
     pokemonList.appendChild(listItem);
-    button.addEventListener('click', function (event){
+    button.addEventListener('click', function(event) {
       showDetails(pokemon)
     });
   }
 
-  function showDetails(pokemon){
-    console.log(pokemon)
+
+  // load pokemomn api
+
+  function loadList() {
+    return fetch(apiUrl).then(function(response) {
+      return response.json();
+    }).then(function(json) {
+      json.results.forEach(function(item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+    }).catch(function(e) {
+      console.error(e);
+    })
   }
+
+  //isolate and fetch the item details to display
+
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url).then(function(response) {
+      return response.json();
+    }).then(function(details) {
+      // Now we add the details to the item
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    }).catch(function(e) {
+      console.error(e);
+    });
+  }
+
+  function showDetails(item) {
+    pokemonRepository.loadDetails(item).then(function() {
+      console.log(item);
+    });
+  }
+
 
   return {
     add: add,
     getAll: getAll,
     addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
     showDetails: showDetails
   };
 })();
 
-pokemonRepository.add({ name: 'Pikachu', height: 0.3, type: ['Electric'] })
 
-pokemonRepository.getAll().forEach(function (pokemon) {
+pokemonRepository.loadList().then(function() {
+  pokemonRepository.getAll().forEach(function(pokemon) {
     pokemonRepository.addListItem(pokemon);
-  })
-
-
-
-// displays height and size of pokemon
-
-// for (let i = 0; i < pokemonList.length; i++) {
-//   if (pokemonList[i].height < 0.9) {
-//     document.write(pokemonList[i].name + " height: " + (pokemonList[i].height) + " small pokemon" + "<br>");
-//   } else if (pokemonList[i].height > 0.9 && pokemonList[i].height < 1.4) {
-//     document.write(pokemonList[i].name + " medium pokemon" + "<br>");
-//   } else {
-//     document.write(pokemonList[i].name + " large pokemon" + "<br>");
-//   }
-// }
+  });
+});
